6c06053bf6d950bf0854b86143d4b2ab3f4ff653
Wenlyu Ye <wenlyye@amazon.com>
2022-11-16 17:00:13 -0800

    [feat] Bootstrap nodets setup

---
+++ src/const.ts
@@ -0,0 +1,4 @@
+// Log prefix
+export const INFO_TAG = "INFO";
+export const DEBUG_TAG = "DEBUG";
+export const ERROR_TAG = "ERROR";
---
+++ src/lambda.ts
@@ -0,0 +1,9 @@
+import { Context, ScheduledEvent } from "aws-lambda";
+import { INFO_TAG } from "./const";
+
+export const lambdaHandler = async (event: ScheduledEvent, context: Context): Promise<any> => {
+    console.log(INFO_TAG + `Recieved Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(INFO_TAG + `Recieved Context: ${JSON.stringify(event, null, 2)}`);
+
+    
+}
