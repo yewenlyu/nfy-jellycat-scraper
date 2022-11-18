1335df367424129c3bbf9ea410883eb0437d9627
Wenlyu Ye <wenlyye@amazon.com>
2022-11-17 18:11:54 -0800

    [feat] Layout architecture

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,21 @@
+// config
+export type PRODUCT_NAME = "Amuseable Avocado Bag" | "Amuseable Happy Boiled Egg Bag" | "Amuseable Toast Bag";
+export const PRODUCT_CHECK_LIST: { productName: PRODUCT_NAME, url: string }[] = [
+    {
+        productName: "Amuseable Avocado Bag",
+        url: "https://www.jellycat.com/us/amuseable-avocado-bag-a4asb/"
+    },
+    {
+        productName: "Amuseable Happy Boiled Egg Bag",
+        url: "https://www.jellycat.com/us/amuseable-happy-boiled-egg-bag-a4be/"
+    },
+    {
+        productName: "Amuseable Toast Bag",
+        url: "https://www.jellycat.com/us/amuseable-toast-bag-a4tb/"
+    }
+];
+
 // Log prefix
-export const INFO_TAG = "INFO";
-export const DEBUG_TAG = "DEBUG";
-export const ERROR_TAG = "ERROR";
+export const INFO_TAG = "[INFO] ";
+export const DEBUG_TAG = "[DEBUG] ";
+export const ERROR_TAG = "[ERROR] ";
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,9 +1,10 @@
 import { Context, ScheduledEvent } from "aws-lambda";
 import { INFO_TAG } from "./const";
 
-export const lambdaHandler = async (event: ScheduledEvent, context: Context): Promise<any> => {
+export const lambdaHandler = async (event: ScheduledEvent, context: Context, callback: () => Promise<any>) => {
     console.log(INFO_TAG + `Recieved Event: ${JSON.stringify(event, null, 2)}`);
     console.log(INFO_TAG + `Recieved Context: ${JSON.stringify(event, null, 2)}`);
 
     
+
 }
